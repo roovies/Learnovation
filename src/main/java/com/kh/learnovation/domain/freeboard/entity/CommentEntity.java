e9fc77f7a49afbec610e4df8d8268fc0c41004ea
이지환 <roovies55@gmail.com>
2023-05-04 17:20:26 +0900

    MERGE : 이지환, 김진하, 설석현

--- src/main/java/com/kh/learnovation/domain/freeboard/entity/CommentEntity.java
+++ src/main/java/com/kh/learnovation/domain/freeboard/entity/CommentEntity.java
@@ -4,6 +4,7 @@ import com.kh.learnovation.domain.freeboard.dto.CommentDTO;
 import com.kh.learnovation.domain.user.entity.User;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -15,6 +16,7 @@ import java.util.List;
 @Getter
 @Setter
 @Table(name = "comment_table")
+@NoArgsConstructor
 public class CommentEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
